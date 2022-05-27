0f0c2da05ede16747f7de17041985aeef820c839
pg4devt <pg4devt@gmail.com>
2022-05-27 13:30:33 +0800

    Added context and charts

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,3 @@
-//1:13:05
-
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { SiShopware } from 'react-icons/si';
@@ -7,11 +5,16 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
-
-import { useStateContext } from './contexts/ContextProvider';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -20,12 +23,11 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       { activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() => {}}
-            className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+          <Link to="/" onClick={handleCloseSideBar} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
             <SiShopware /><span>Shoppy</span>
           </Link>          
           <TooltipComponent content='Menu' position='BottomCenter'>            
-            <button type='button' onClick={() => {}}
+            <button type='button' onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
                className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
               <MdOutlineCancel />
             </button>
@@ -41,7 +43,7 @@ const Sidebar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={link.name}
-                  onClick={() => {}}
+                  onClick={handleCloseSideBar}
                   className={({ isActive }) => isActive ? activeLink : normalLink}>
                     {link.icon}
                     <span className='capitalize'>
@@ -58,4 +60,3 @@ const Sidebar = () => {
 }
 
 export default Sidebar
-// 53:14
